4aef9426a66a35d1b9e5d2f818baa628eed211b3
DURAND Boris <boris@pop-os.localdomain>
2021-02-11 11:51:51 +0100

    Iniatialisation_2

--- arduino.ino
+++ arduino.ino
@@ -4,12 +4,12 @@
 #   Fichier du programme pour l'arduino
 */
 
-void setup() {
-  // put your setup code here, to run once:
+void setup() 
+{
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
 
 }
