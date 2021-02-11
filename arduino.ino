f44aea037a5553a2cd2f704128a2a9fc45e22a88
DURAND Boris <durand.boris@outlook.com>
2021-02-11 20:36:06 +0100

    Intégration de l'écran LCD au programme

--- arduino.ino
+++ arduino.ino
@@ -4,12 +4,22 @@
 #   Fichier du programme pour l'arduino
 */
 
+#include LiquideCristal_I2C lcd(0x27, 16, 2);// définit le type d'écran lcd pour ma part j'utilise un 16 x 2
+
+const int PIN_CAPTEUR_TEMPERATURE =10;
+const int PIN_CAPTEUR_HUMIDITE = 11;
+
 void setup() 
 {
-
+    Serial.begin(9600);
+    lcd.begin(); 
+    lcd.backlight();
 }
 
 void loop() 
 {
-
+    lcd.clear(); 
+    lcd.setCursor(0,0); // positionne le curseur à la colonne 1 et à la ligne 2 
+    lcd.print("Hello World ! "); 
+    delay(1000);
 }
